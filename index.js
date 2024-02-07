368d5903e7a98ee1aae2268bb1d028174ad50a25
johngaalt <rajon777666@gmail.com>
2024-02-07 12:10:15 +0300

    feat: vercel configuration

--- index.js
+++ index.js
@@ -1,40 +1,8 @@
 const jsonServer = require("json-server");
 const cors = require("cors");
 
-const db = {
-  garage: [
-    {
-      name: "Tesla",
-      color: "#e6e6fa",
-      id: 1,
-    },
-    {
-      name: "BMW",
-      color: "#fede00",
-      id: 2,
-    },
-    {
-      name: "Mersedes",
-      color: "#6c779f",
-      id: 3,
-    },
-    {
-      name: "Ford",
-      color: "#ef3c40",
-      id: 4,
-    },
-  ],
-  winners: [
-    {
-      id: 1,
-      wins: 1,
-      time: 10,
-    },
-  ],
-};
-
 const server = jsonServer.create();
-const router = jsonServer.router(db);
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 const PORT = 3000;
