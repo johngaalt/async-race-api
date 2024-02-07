f26acf68cc85c3177df99ec2caffe0b86461b954
johngaalt <rajon777666@gmail.com>
2024-02-07 11:37:47 +0300

    refactor: disable cors

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
+const cors = require("cors");
 
 const db = {
-    garage: [
-        {
-            "name": "Tesla",
-            "color": "#e6e6fa",
-            "id": 1,
-        },
-        {
-            "name": "BMW",
-            "color": "#fede00",
-            "id": 2,
-        },
-        {
-            "name": "Mersedes",
-            "color": "#6c779f",
-            "id": 3,
-        },
-        {
-            "name": "Ford",
-            "color": "#ef3c40",
-            "id": 4,
-        },
-    ],
-    winners: [
-        {
-            id: 1,
-            wins: 1,
-            time: 10,
-        }
-    ]
+  garage: [
+    {
+      name: "Tesla",
+      color: "#e6e6fa",
+      id: 1,
+    },
+    {
+      name: "BMW",
+      color: "#fede00",
+      id: 2,
+    },
+    {
+      name: "Mersedes",
+      color: "#6c779f",
+      id: 3,
+    },
+    {
+      name: "Ford",
+      color: "#ef3c40",
+      id: 4,
+    },
+  ],
+  winners: [
+    {
+      id: 1,
+      wins: 1,
+      time: 10,
+    },
+  ],
 };
 
 const server = jsonServer.create();
@@ -42,58 +43,90 @@ const state = { velocity: {}, blocked: {} };
 
 server.use(middlewares);
 
-server.patch('/engine', (req, res) => {
-    const { id, status } = req.query;
+server.use(cors());
 
-    if (!id || !status || !/^(started)|(stopped)|(drive)$/.test(status)) {
-        return res.status(400).send('Wrong parameters: "id" should be any positive number, "status" should be "started", "stopped" or "drive"');
-    }
+server.patch("/engine", (req, res) => {
+  const { id, status } = req.query;
 
-    if (!db.garage.find(car => car.id === +id)) {
-        return res.status(404).send('Car with such id was not found in the garage.')
-    }
+  if (!id || !status || !/^(started)|(stopped)|(drive)$/.test(status)) {
+    return res
+      .status(400)
+      .send(
+        'Wrong parameters: "id" should be any positive number, "status" should be "started", "stopped" or "drive"',
+      );
+  }
 
-    const distance = 500000;
-    if (status === 'drive') {
-        const velocity = state.velocity[id];
+  if (!db.garage.find((car) => car.id === +id)) {
+    return res
+      .status(404)
+      .send("Car with such id was not found in the garage.");
+  }
 
-        if (!velocity) return res.status(404).send('Engine parameters for car with such id was not found in the garage. Have you tried to set engine status to "started" before?');
-        if (state.blocked[id]) return res.status(429).send('Drive already in progress. You can\'t run drive for the same car twice while it\'s not stopped.');
-        
-        state.blocked[id] = true;
+  const distance = 500000;
+  if (status === "drive") {
+    const velocity = state.velocity[id];
 
-        const x = Math.round(distance / velocity);
+    if (!velocity)
+      return res
+        .status(404)
+        .send(
+          'Engine parameters for car with such id was not found in the garage. Have you tried to set engine status to "started" before?',
+        );
+    if (state.blocked[id])
+      return res
+        .status(429)
+        .send(
+          "Drive already in progress. You can't run drive for the same car twice while it's not stopped.",
+        );
 
-        if (new Date().getMilliseconds() % 3 === 0) {
-            setTimeout(() => {
-                delete state.velocity[id];
-                delete state.blocked[id];
-                res.header('Content-Type', 'application/json').status(500).send('Car has been stopped suddenly. It\'s engine was broken down.');
-            }, Math.random() * x ^ 0);
-        } else {
-            setTimeout(() => {
-                delete state.velocity[id];
-                delete state.blocked[id];
-                res.header('Content-Type', 'application/json').status(200).send(JSON.stringify({ success: true }));
-            }, x);
-        }
+    state.blocked[id] = true;
+
+    const x = Math.round(distance / velocity);
+
+    if (new Date().getMilliseconds() % 3 === 0) {
+      setTimeout(() => {
+        delete state.velocity[id];
+        delete state.blocked[id];
+        res
+          .header("Content-Type", "application/json")
+          .status(500)
+          .send("Car has been stopped suddenly. It's engine was broken down.");
+      }, (Math.random() * x) ^ 0);
     } else {
-        const x = req.query.speed ? +req.query.speed : Math.random() * 2000 ^ 0;
-
-        const velocity = status === 'started' ? Math.max(50, Math.random() * 200 ^ 0) : 0;
-
-        if (velocity) {
-            state.velocity[id] = velocity;
-        } else {
-            delete state.velocity[id];
-            delete state.blocked[id];
-        }
-
-        setTimeout(() => res.header('Content-Type', 'application/json').status(200).send(JSON.stringify({ velocity, distance })), x);
+      setTimeout(() => {
+        delete state.velocity[id];
+        delete state.blocked[id];
+        res
+          .header("Content-Type", "application/json")
+          .status(200)
+          .send(JSON.stringify({ success: true }));
+      }, x);
     }
+  } else {
+    const x = req.query.speed ? +req.query.speed : (Math.random() * 2000) ^ 0;
+
+    const velocity =
+      status === "started" ? Math.max(50, (Math.random() * 200) ^ 0) : 0;
+
+    if (velocity) {
+      state.velocity[id] = velocity;
+    } else {
+      delete state.velocity[id];
+      delete state.blocked[id];
+    }
+
+    setTimeout(
+      () =>
+        res
+          .header("Content-Type", "application/json")
+          .status(200)
+          .send(JSON.stringify({ velocity, distance })),
+      x,
+    );
+  }
 });
 
 server.use(router);
 server.listen(PORT, () => {
-    console.log('Server is running on port', PORT);
+  console.log("Server is running on port", PORT);
 });
